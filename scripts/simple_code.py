e6c6e5cb7a6c59bea97a57d0a7f33c01b8fbeb24
Furkan Edizkan <furkandedizkan@gmail.com>
2022-09-04 15:30:18 +0300

    Subject: Updated simple_code.py Details: Fixed pylint erros on simple_code.py & ignored a file Task-ID: Co-authored-by:

--- scripts/simple_code.py
+++ scripts/simple_code.py
@@ -1,22 +1,28 @@
+"""
+Random hash generator
+"""
+
+import string
 import random
 import os
+import hashlib
+import uuid
 
-"""
-Create random number and writes into a log file
-"""
-
-text_file_path = os.path.join(os.getcwd(), "hash_log.txt")
+FILE_PATH = os.path.join(os.getcwd(), "hash_log.txt")
 
 def generate_hash_and_write():
-    
-    for i in range(5):
-        hash = random.getrandbits(64)
-        print("{}. Hash Values == %16x".format(i+1) % hash)
+    """ Random hash generator function
+    Creates random value and save it into a file
+    """
+    letters = string.ascii_letters
+    seed = ''.join(random.choice(letters) for i in range(10))
+    md5_hash = hashlib.md5()
+    md5_hash.update(seed.encode('utf-8'))
+    new_uuid = uuid.UUID(md5_hash.hexdigest())
+    print("Hash Values == %s" % new_uuid)
 
-    f = open(str(text_file_path), "a")
-    f.write(str(hash)+ "\n")
+    with open(str(FILE_PATH), "a", encoding='utf-8') as hash_file:
+        hash_file.write(str(new_uuid)+ "\n")
 
 if __name__ == '__main__':
     generate_hash_and_write()
-# Rebase Changes
-# main changes
